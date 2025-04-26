c8f8af57e8a6364db0683a97b68655c140fcfdbb
Kazak <strannik19742@mail.ru>
2025-04-26 22:08:25 +0300

    4 день

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,5 +1,6 @@
 const getGoods = function () {
   const links = document.querySelectorAll(".navigation-link");
+  const more = document.querySelector(".more");
 
   const renderGoods = (goods) => {
     const goodsContainer = document.querySelector(".long-goods-list");
@@ -58,6 +59,13 @@ const getGoods = function () {
     window.location.pathname === "/goods.html"
   ) {
     renderGoods(JSON.parse(localStorage.getItem("goods")));
+  }
+  if (more) {
+    more.addEventListener("click", () => {
+      event.preventDefault();
+
+      getData();
+    });
   }
 };
 getGoods();
